8b3a3f59b519a033866dae72fd341f3253fc54c1
shubhamjoshi1303 <46135499+shubhamjoshi1303@users.noreply.github.com>
2026-05-25 06:40:32 -0400

    Add 1_two_sum.py - LeetCode Problem 1

---
+++ 1_two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted(nums)
+        hash_map = {}
+
+        for i,num in enumerate(nums) :
+            if target - num in hash_map:
+                return [hash_map[target-num], i]
+            hash_map[num] = i
