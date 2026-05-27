7afc272b8a368c88e83aa6c79fc3268e08d4bd62
Shubham Joshi <shubhamjoshi1303@gmail.com>
2026-05-27 10:19:42 -0400

    Update 206_reverse_linked_list.py - LeetCode Problem 206

--- 206_reverse_linked_list.py
+++ 206_reverse_linked_list.py
@@ -4,9 +4,19 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode],prev = None ) -> Optional[ListNode]:
-        if head is None :
-            return prev
-        next = head.next
-        head.next = prev
-        return self.reverseList(next,head)
+    # def reverseList(self, head: Optional[ListNode],prev = None ) -> Optional[ListNode]:
+    #     if head is None :
+    #         return prev
+    #     next = head.next
+    #     head.next = prev
+    #     return self.reverseList(next,head)
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None 
+        current = head 
+
+        while current is not None :
+            next = current.next
+            current.next = prev
+            prev = current 
+            current = next
+        return prev
