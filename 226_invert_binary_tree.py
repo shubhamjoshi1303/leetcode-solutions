636f1f9527cc4d624ad287de58bf34d9ad2d0997
Shubham Joshi <shubhamjoshi1303@gmail.com>
2026-05-28 20:27:16 -0400

    Update 226_invert_binary_tree.py - LeetCode Problem 226

--- 226_invert_binary_tree.py
+++ 226_invert_binary_tree.py
@@ -6,22 +6,35 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root :
-            return None
+
+        if not root:
+            return 
         
-        stack = [root]
-        values = []
+        temp = root.left
+        root.left = root.right
+        root.right = temp
 
-        while stack:
-            node = stack.pop()
-            values.append(node.val)
-
-            temp = node.left
-            node.left = node.right
-            node.right = temp
-
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
+
+        
+        # if not root :
+        #     return None
+        
+        # stack = [root]
+        # values = []
+
+        # while stack:
+        #     node = stack.pop()
+        #     values.append(node.val)
+
+        #     temp = node.left
+        #     node.left = node.right
+        #     node.right = temp
+
+        #     if node.left:
+        #         stack.append(node.left)
+        #     if node.right:
+        #         stack.append(node.right)
+        # return root
