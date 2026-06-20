d5286fbb419fea0c891c942c49accc287b993a10
Shubham Joshi <shubhamjoshi1303@gmail.com>
2026-06-19 20:56:50 -0400

    Update 15_3sum.py - LeetCode Problem 15

--- 15_3sum.py
+++ 15_3sum.py
@@ -1,24 +1,23 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        res = []
         nums.sort()
-
+        res = []
         for i,n in enumerate(nums):
             if i > 0 and n == nums[i-1]:
-                continue 
-            
-            l,r = i+1 , len(nums)-1
+                continue
 
-            while l < r :
-                threeSum = n + nums[l]+ nums[r]
+            l,r = i+1, len(nums)-1
 
-                if threeSum > 0 :
+            while l<r:
+                Sum = n + nums[l] + nums[r]
+
+                if Sum < 0 :
+                    l+=1
+                elif Sum>0:
                     r-=1
-                elif threeSum < 0 :
+                else:
+                    res.append([n , nums[l],nums[r]])
                     l+=1
-                else :
-                    res.append([n,nums[l],nums[r]])
-                    l+=1
-                    while nums[l] == nums[l-1] and l < r:
+                    while l<r and nums[l] == nums[l-1]:
                         l+=1
         return res
