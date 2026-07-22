dcd071c5168731ddcf9536f7478484c5160620e9
Shubham Joshi <shubhamjoshi1303@gmail.com>
2026-07-22 03:02:34 -0400

    Update 1_two_sum.py - LeetCode Problem 1

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,8 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        table = {}
-        for i,num in enumerate(nums):
-            if target - num in table:
-                return [table[target- num], i ]
-            if num not in table:
-                table[num] = i
+        prevMap = {}
+
+        for i , n in enumerate(nums):
+            diff = target - n 
+            if diff in prevMap :
+                return [i,prevMap[diff]]
+            prevMap[n] = i 
+        return
